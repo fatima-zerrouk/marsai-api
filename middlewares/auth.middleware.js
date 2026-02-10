63573ae983b8c50d67484322103eb95a11885971
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-02-10 16:30:30 +0100

    mise a jour serveur.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,21 +1,19 @@
-
-
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
 export const authenticate = (req, res, next) => {
-  const authHeader = req.headers.authorization
+  const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Token manquant ou invalide' })
+    return res.status(401).json({ message: 'Token manquant ou invalide' });
   }
 
-  const token = authHeader.split(' ')[1]
+  const token = authHeader.split(' ')[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    req.user = decoded 
-    next()
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
   } catch (err) {
-    return res.status(401).json({ message: 'Token invalide ou expiré' })
+    return res.status(401).json({ message: 'Token invalide ou expiré' });
   }
-}
+};
